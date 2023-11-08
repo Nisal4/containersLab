85ca28f625deca8314128a9b253f9ed2979df6dc
Nisal <nisatt11@gmail.com>
2023-11-08 17:22:46 -0500

    Third and Fourth Problems

--- containers.py
+++ containers.py
@@ -22,14 +22,27 @@ for food in foods:
     print(f'{food} is a good food')
 
 # Exercise 3
+# Using a for loop, print just the last two food strings from foods.
 
 # your solution here
 
+for food in foods[-2:]:
+    print(food)
 
 # Exercise 4
+# Create a dictionary named home_town containing the keys of city, state and population.
+# Print a string with this format:
+# "I was born in city, state - population of population"
 
 # your solution here
 
+home_town = {
+    "city": "New York", 
+    "state": "NY", 
+    "population": 7888121
+}
+
+print(f'I was born in {home_town["city"]}, {home_town["state"]} - population of {home_town["population"]}')
 
 # Exercise 5
 
