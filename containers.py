a9db72f5994ed152c260b1a19809d7c4da3a24ab
Nisal <nisatt11@gmail.com>
2023-11-08 17:05:37 -0500

    First Problem

---
+++ containers.py
@@ -0,0 +1,45 @@
+# Exercise 1
+# Create a list named students containing some student names (strings).
+# Print out the second student's name.
+# Print out the last student's name.
+
+# your solution here
+
+students = ["Nisal", "Nick", "Krystalin", "Chris", "Ahmad", "Kevin"]
+
+print(students[1])
+print(students[-1])
+
+# Exercise 2
+
+# your solution here
+
+
+# Exercise 3
+
+# your solution here
+
+
+# Exercise 4
+
+# your solution here
+
+
+# Exercise 5
+
+# your solution here
+
+
+# Exercise 6
+
+# your solution here
+
+
+# Exercise 7
+
+# your solution here
+
+
+# Exercise 8
+
+# your solution here
